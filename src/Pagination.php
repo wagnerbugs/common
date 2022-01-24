8622235873f76ed402a397578b7ae1d0e9c4053c
Wagner Bugs <wagnerbugs@gmail.com>
2022-01-24 00:05:00 -0300

    Update Pagination.php
    Limpando código

--- src/Pagination.php
+++ src/Pagination.php
@@ -4,36 +4,11 @@ namespace WagnerBugs\DatabaseManager;
 
 class Pagination
 {
-    /**
-     * Número máximo de registros por página
-     * @var integer
-     */
     private $limit;
-
-    /**
-     * Quantidade total de resultados do banco de dados
-     * @var integer
-     */
     private $results;
-
-    /**
-     * Quantidade de páginas
-     * @var integer
-     */
     private $pages;
-
-    /**
-     * Página atual
-     * @var integer
-     */
     private $currentPage;
 
-    /**
-     * Construtor da classe
-     * @param integer  $results
-     * @param integer  $currentPage
-     * @param integer  $limit
-     */
     public function __construct($results, $currentPage = 1, $limit = 10)
     {
         $this->results     = $results;
@@ -42,9 +17,6 @@ class Pagination
         $this->calculate();
     }
 
-    /**
-     * Função que calcula a páginação
-     */
     private function calculate()
     {
         //Calcula o total de páginas
@@ -54,28 +26,18 @@ class Pagination
         $this->currentPage = $this->currentPage <= $this->pages ? $this->currentPage : $this->pages;
     }
 
-    /**
-     * Função que retorna a cláusula limit da SQL
-     * @return string
-     */
     public function getLimit()
     {
         $offset = ($this->limit * ($this->currentPage - 1));
         return $offset.','.$this->limit;
     }
 
-    /**
-     * Função que retorna as opções de páginas disponíveis
-     * @return array
-     */
     public function getPages()
     {
-        //Não retorna páginas
         if ($this->pages == 1) {
             return [];
         }
 
-        //Páginas
         $pages = [];
         for ($i = 1; $i <= $this->pages; $i++) {
             $pages[] = [
