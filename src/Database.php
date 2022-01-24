9b235c75f384c905a0d249ae4e1c45f19a05c1a2
Wagner Bugs <wagnerbugs@gmail.com>
2022-01-24 00:03:44 -0300

    Update Database.php
    Tirando as linhas comentadas

--- src/Database.php
+++ src/Database.php
@@ -7,56 +7,15 @@ use PDOException;
 
 class Database
 {
-    /**
-     * Host de conexão com o banco de dados
-     * @var string
-     */
     private static $host;
-
-    /**
-     * Nome do banco de dados
-     * @var string
-     */
     private static $name;
-
-    /**
-     * Usuário do banco
-     * @var string
-     */
     private static $user;
-
-    /**
-     * Senha de acesso ao banco de dados
-     * @var string
-     */
     private static $pass;
-
-    /**
-     * Porta de acesso ao banco de dados
-     * @var integer
-     */
     private static $port;
-
-    /**
-     * Nome da tabela a ser manipulada
-     * @var string
-     */
+    
     private $table;
-
-    /**
-     * Instância de conexão com o banco de dados
-     * @var PDO
-     */
     private $connection;
 
-    /**
-     * Função que configura a classe
-     * @param  string  $host
-     * @param  string  $name
-     * @param  string  $user
-     * @param  string  $pass
-     * @param  integer $port
-     */
     public static function config($host, $name, $user, $pass, $port = 3306)
     {
         self::$host = $host;
@@ -66,19 +25,12 @@ class Database
         self::$port = $port;
     }
 
-    /**
-     * Contrutor que define a tabela, instancia de conexão
-     * @param string $table
-     */
     public function __construct($table = null)
     {
         $this->table = $table;
         $this->setConnection();
     }
 
-    /**
-     * Função que cria uma conexão com o banco de dados
-     */
     private function setConnection()
     {
         try {
@@ -89,12 +41,6 @@ class Database
         }
     }
 
-    /**
-     * Função que executa queries dentro do banco de dados
-     * @param  string $query
-     * @param  array  $params
-     * @return PDOStatement
-     */
     public function execute($query, $params = [])
     {
         try {
@@ -106,84 +52,36 @@ class Database
         }
     }
 
-    /**
-     * Função que insere dados no banco de dados
-     * @param  array $values [ field => value ]
-     * @return integer ID inserido
-     */
     public function insert($values)
     {
-        //Dados da query
         $fields = array_keys($values);
         $binds  = array_pad([], count($fields), '?');
-
-        //Monta a query
         $query = 'INSERT INTO '.$this->table.' ('.implode(',', $fields).') VALUES ('.implode(',', $binds).')';
-
-        //Executa o insert
         $this->execute($query, array_values($values));
-
-        //Retorna o id inserido
         return $this->connection->lastInsertId();
     }
 
-    /**
-     * Função que executa uma consulta no banco
-     * @param  string $where
-     * @param  string $order
-     * @param  string $limit
-     * @param  string $fields
-     * @return PDOStatement
-     */
     public function select($where = null, $order = null, $limit = null, $fields = '*')
     {
-        //Dados da query
         $where = strlen($where) ? 'WHERE '.$where : '';
         $order = strlen($order) ? 'ORDER BY '.$order : '';
         $limit = strlen($limit) ? 'LIMIT '.$limit : '';
-
-        //Monta a query
         $query = 'SELECT '.$fields.' FROM '.$this->table.' '.$where.' '.$order.' '.$limit;
-
-        //Executa a query
         return $this->execute($query);
     }
 
-    /**
-     * Função que executa atualizações no banco de dados
-     * @param  string $where
-     * @param  array $values [ field => value ]
-     * @return boolean
-     */
     public function update($where, $values)
     {
-        //Dados da query
         $fields = array_keys($values);
-
-        //Monta a query
         $query = 'UPDATE '.$this->table.' SET '.implode('=?,', $fields).'=? WHERE '.$where;
-
-        //Executa a query
         $this->execute($query, array_values($values));
-
-        //Retorna sucesso
         return true;
     }
 
-    /**
-     * Função que exclui dados do banco de dados
-     * @param  string $where
-     * @return boolean
-     */
     public function delete($where)
     {
-        //Monta a query
         $query = 'DELETE FROM '.$this->table.' WHERE '.$where;
-
-        //Executa a query
         $this->execute($query);
-
-        //Retorna sucesso
         return true;
     }
 }
